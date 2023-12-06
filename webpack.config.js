6f68689aed36c4f96ab6295d4426592dd8eaf8d0
Makara <152614418+soreungmakara2@users.noreply.github.com>
2023-12-06 21:48:23 +0700

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "",
+    filename: "bundle.[fullhash].js",
+    clean: true, // removes unused files from output dir
+  },
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      // Need to use template because need 'root' div for react injection. templateContent doesn't play nice with title, so just use a template file instead.
+      template: "./src/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "./src/images/favicon.png", to: "./favicon.png" },
+      ],
+      options: {
+        concurrency: 100,
+      },
+    }),
+  ],
+  performance: {
+    maxEntrypointSize: 280000,
+    maxAssetSize: 280000
+  },
+};
